9a08ad4ff1f3dc9a8c711b369df6a3e2caf22c55
RobbinThakur <101439452+RobbinThakur@users.noreply.github.com>
2023-02-25 12:02:59 +0530

    Add files via upload

---
+++ buton.js
@@ -0,0 +1,9 @@
+<!-- Create a button named "Check Eligibility" -->
+<button onclick="redirectToEligibility()">Check Eligibility</button>
+
+<!-- JavaScript function to redirect to the "eligibility" webpage -->
+<script>
+function redirectToEligibility() {
+  window.location.href = "eligibility.html";
+}
+</script>
